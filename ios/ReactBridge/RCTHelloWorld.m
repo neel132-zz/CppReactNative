28ecd8ce3074eeea9a43a604151c50745d589c30
Neel Gala <neel@theinnerhour.com>
2019-03-27 11:13:53 +0530

    Initial Commit

---
+++ ios/ReactBridge/RCTHelloWorld.m
@@ -0,0 +1,26 @@
+#import "RCTHelloWorld.h"
+#import "HWHelloWorld.h"
+
+@implementation RCTHelloWorld{
+    HWHelloWorld *_cppApi;
+}
+- (RCTHelloWorld *)init
+{
+    self = [super init];
+    _cppApi = [HWHelloWorld create];
+    return self;
+}
++ (BOOL)requiresMainQueueSetup
+{
+    return NO;
+}
+RCT_EXPORT_MODULE();
+
+RCT_REMAP_METHOD(sayHello,
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    NSString *response = [_cppApi getHelloWorld];
+    resolve(response);
+}
+@end
---
+++ src/cpp/hello_world_impl.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "hello_world.hpp"
+
+namespace helloworld {
+    class HelloWorldImpl : public helloworld::HelloWorld {
+
+    public:
+
+        //Constructor
+        HelloWorldImpl();
+
+        //Our method that returns a string
+        std::string get_hello_world();
+    };
+
+}
